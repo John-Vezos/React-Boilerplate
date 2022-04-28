b8cdbf84ac94f4f92a54df6de4f44fab56025bfb
John-Vezos <vezos@hotmail.gr>
2022-04-28 19:15:09 +0300

    initial

--- src/containers/ThemeToggle/index.tsx
+++ src/containers/ThemeToggle/index.tsx
@@ -2,9 +2,8 @@ import { FunctionComponent } from 'react';
 import { DefaultRootState, useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from 'redux';
 
+import SunAndMoon from '@src/components/SunAndMoon';
 import { themeHandler } from '@src/Redux/UI/uiActions';
-
-import SunAndMoon from './../../components/SunAndMoon';
 
 const ThemeToggle: FunctionComponent = () => {
   const isLightTheme: boolean = useSelector(
