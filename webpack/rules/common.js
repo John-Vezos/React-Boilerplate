4a5460ec5b1cd0a231c008d7946447c5c1d518ea
John-Vezos <vezos@hotmail.gr>
2022-05-05 12:46:30 +0300

    Update common.js

--- webpack/rules/common.js
+++ webpack/rules/common.js
@@ -53,3 +53,13 @@ export const cssRule = {
   include: /node_modules/,
   use: ['style-loader', 'css-loader'],
 };
+
+export const cssRuleCustom = {
+  // Preprocess our own .css files
+  // This is the place to add your own loaders (e.g. sass/less etc.)
+  // for a list of loaders, see https://webpack.js.org/loaders/#styling
+  test: /\.css$/,
+  exclude: /node_modules/,
+  use: ['style-loader', 'css-loader'],
+};
+export const cssRules = [cssRule, cssRuleCustom];
