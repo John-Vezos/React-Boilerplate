5eb48c6831aa72233e7cfe63fcd127241e8da258
John-Vezos <vezos@hotmail.gr>
2022-05-05 17:29:27 +0300

    Update RestoreScrollPosition.tsx

--- src/hooks/RestoreScrollPosition.tsx
+++ src/hooks/RestoreScrollPosition.tsx
@@ -1,23 +1,16 @@
-import {FunctionComponent, useEffect} from 'react';
-import {useLocation} from 'react-router-dom';
-import {animateScroll} from 'react-scroll';
+import { FunctionComponent, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { animateScroll } from 'react-scroll';
 
-let prevLocation = '';
 const exceptionLocation: string | string[] = [];
 
 const RestoreScrollPosition: FunctionComponent = (): null => {
-  const {pathname} = useLocation();
-
-  const scrollConditions = () => {
-    return !(
-      exceptionLocation.includes(pathname) &&
-      exceptionLocation.includes(prevLocation)
-    );
-  };
+  const { pathname } = useLocation();
 
   useEffect(() => {
     window.history.scrollRestoration = 'manual';
     window.addEventListener('beforeunload', windowBeforeUnloadHandler);
+
     const scrollY = localStorage.getItem('scrollBar');
     if (scrollY) {
       animateScroll.scrollTo(Number(scrollY), {
@@ -28,14 +21,21 @@ const RestoreScrollPosition: FunctionComponent = (): null => {
       });
 
       localStorage.removeItem('scrollBar');
-    } else if (scrollConditions()) {
-      window.scrollTo(0, 0);
     }
-    prevLocation = pathname;
 
     return () =>
       window.removeEventListener('beforeunload', windowBeforeUnloadHandler);
+  }, []);
+
+  useEffect(() => {
+    if (scrollConditions()) {
+      window.scrollTo(0, 0);
+    }
   }, [pathname]);
+
+  const scrollConditions = () => {
+    return !exceptionLocation.includes(pathname);
+  };
 
   const windowBeforeUnloadHandler = () =>
     localStorage.setItem('scrollBar', window.scrollY.toString());
