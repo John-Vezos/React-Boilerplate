71a89be98844c9930fbeb9c8497836017433c4fb
John-Vezos <vezos@hotmail.gr>
2022-05-06 10:56:34 +0300

    Update index.tsx

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,10 +1,23 @@
-import { FormEvent, FunctionComponent } from 'react';
+import { FunctionComponent, InputHTMLAttributes } from 'react';
 
 import Label from '@components/TextField/Label';
 import outlinedStyle from '@components/TextField/outlinedStyle';
 import Wrapper from '@components/TextField/Wrapper';
 
 import styled from '@styles/styled-components';
+
+// type Modify<T, R> = Omit<T, keyof R> & R;
+//
+// interface ModifiedType
+//   extends Modify<
+//     InputHTMLAttributes<HTMLInputElement>,
+//     {
+//       label?: string;
+//       onChange?: (text: string) => void;
+//       maxWidth?: string;
+//       variant?: string;
+//     }
+//   > {}
 
 interface PropsName {
   variant?: string;
@@ -13,40 +26,21 @@ const InputStyle = styled.input<PropsName>`
   ${props => props.variant === 'outlined' && outlinedStyle}
 `;
 
-interface Props {
-  id?: string;
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
-  type?: string;
-  value?: string;
-  onChange?: (text: string) => void;
-  required?: boolean;
-  placeholder?: string;
   maxWidth?: string;
   variant?: string;
 }
+
 const TextField: FunctionComponent<Props> = ({
-  id,
   label,
-  type,
-  value,
-  onChange,
-  placeholder,
-  required,
   maxWidth,
   variant,
+  ...props
 }) => (
   <Wrapper maxWidth={maxWidth}>
-    <InputStyle
-      id={id}
-      type={type}
-      name={'test'}
-      value={value}
-      onChange={event => onChange && onChange(event.target.value)}
-      placeholder={placeholder}
-      required={required}
-      variant={variant}
-    />
-    <Label htmlFor={id}>{label}</Label>
+    <InputStyle {...props} variant={variant} />
+    <Label htmlFor={props?.id}>{label}</Label>
   </Wrapper>
 );
 
