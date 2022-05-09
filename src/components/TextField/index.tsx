cf42749d5bb99d4e42b3fb4ba2093f5b4a4e42e3
John-Vezos <vezos@hotmail.gr>
2022-05-09 12:24:13 +0300

    Update index.tsx

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, TextareaHTMLAttributes, forwardRef } from 'react';
 
 import Label from '@components/TextField/Label';
 import outlinedStyle from '@components/TextField/outlinedStyle';
@@ -26,22 +26,36 @@ const InputStyle = styled.input<PropsName>`
   ${props => props.variant === 'outlined' && outlinedStyle}
 `;
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
+const TextareaStyle = styled.textarea<PropsName>`
+  ${props => props.variant === 'outlined' && outlinedStyle}
+`;
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   maxWidth?: string;
   variant?: string;
+  multiline?: boolean;
 }
 
-const TextField: FunctionComponent<Props> = ({
-  label,
-  maxWidth,
-  variant,
-  ...props
-}) => (
-  <Wrapper maxWidth={maxWidth}>
-    <InputStyle {...props} variant={variant} />
-    <Label htmlFor={props?.id}>{label}</Label>
-  </Wrapper>
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label?: string;
+  maxWidth?: string;
+  variant?: string;
+  multiline?: boolean;
+}
+
+const TextField = forwardRef<any, InputProps & TextareaProps>(
+  ({ label, maxWidth, variant, multiline, ...props }, ref) => (
+    <Wrapper maxWidth={maxWidth}>
+      {!multiline && <InputStyle {...props} variant={variant} ref={ref} />}
+
+      {multiline && (
+        <TextareaStyle {...props} ref={ref} rows={5} maxLength={500} />
+      )}
+
+      <Label htmlFor={props?.id}>{label}</Label>
+    </Wrapper>
+  ),
 );
 
 export default TextField;
