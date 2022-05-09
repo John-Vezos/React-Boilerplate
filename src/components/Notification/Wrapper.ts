685950fe2f67c6ca18f85321f206bbf7ddf5d187
John-Vezos <vezos@hotmail.gr>
2022-05-09 17:57:29 +0300

    Update Wrapper.ts

--- src/components/Notification/Wrapper.ts
+++ src/components/Notification/Wrapper.ts
@@ -4,35 +4,85 @@ interface Props {
 }
 
 export const Wrapper = styled.section<Props>`
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  position: fixed;
+  top: 0;
   width: 100%;
-  height: 3rem;
   padding: 0.5rem 10%;
-  color: white;
-  background-color: #1A8ED1;
-  
-  &  h1,
-  & p {
-    margin: 0;
-    font-size: 1rem;
+  margin: 0;
+  font-size: 1rem;
+  cursor: pointer;
+
+  border: 1px solid rgba(6, 44, 241, 0.46);
+  background-color: rgba(7, 73, 149, 0.12156862745098039);
+  box-shadow: 0 0 2px #0396ff;
+  color: #0396ff;
+  text-shadow: 0.5px 0.5px #00040a;
+
+  &:hover {
+    background-color: rgba(7, 73, 149, 0.35);
   }
+
+  & strong {
+    margin-right: 5px;
+  }
+
   ${props =>
     props.status === 'success' &&
     css`
-      & {
-        background-color: #1ad1b9;
+      &&& {
+        border: 1px solid rgba(36, 241, 6, 0.46);
+        background-color: rgba(7, 149, 66, 0.12156862745098039);
+        box-shadow: 0 0 2px #259c08;
+        color: #0ad406;
+      }
+
+      &&&:hover {
+        background-color: rgba(7, 149, 66, 0.35);
       }
     `};
-  
-    ${props =>
-      props.status === 'error' &&
-      css`
-        & {
-          background-color: #690000;
-        }
-      `};
-  }
+
+  ${props =>
+    props.status === 'error' &&
+    css`
+      &&& {
+        border: 1px solid rgba(241, 6, 6, 0.81);
+        background-color: rgba(220, 17, 1, 0.16);
+        box-shadow: 0 0 2px #ff0303;
+        color: #ff0303;
+      }
+
+      &&&:hover {
+        background-color: rgba(220, 17, 1, 0.33);
+      }
+    `};
+
+  ${props =>
+    props.status === 'warning' &&
+    css`
+      &&& {
+        border: 1px solid rgba(241, 142, 6, 0.81);
+        background-color: rgba(220, 128, 1, 0.16);
+        box-shadow: 0 0 2px #ffb103;
+        color: #ffb103;
+      }
+
+      &&&:hover {
+        background-color: rgba(220, 128, 1, 0.33);
+      }
+    `};
+
+  ${props =>
+    props.status === 'well' &&
+    css`
+      &&& {
+        border: 1px solid rgba(6, 241, 226, 0.81);
+        background-color: rgba(1, 204, 220, 0.16);
+        box-shadow: 0 0 2px #03fff5;
+        color: #03d0ff;
+      }
+
+      &&&:hover {
+        background-color: rgba(1, 204, 220, 0.33);
+      }
+    `};
 `;
