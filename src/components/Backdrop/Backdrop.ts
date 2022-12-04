c182cb9be15e9aacddfad8f8099ea8835dc4edc0
John-Vezos <vezos@hotmail.gr>
2022-12-04 17:58:32 +0200

    Update Backdrop.ts

--- src/components/Backdrop/Backdrop.ts
+++ src/components/Backdrop/Backdrop.ts
@@ -2,10 +2,7 @@ import styled from '@styles/styled-components';
 
 const Backdrop = styled.div`
   position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+  inset: 0;
 `;
 
 export default Backdrop;
