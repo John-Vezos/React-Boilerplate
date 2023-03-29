9ad7b060649a4fef61191ebbfe8769e4d872b70a
John-Vezos <vezos@hotmail.gr>
2023-03-29 03:37:52 +0300

    Update index.tsx

--- src/containers/ThemeToggle/index.tsx
+++ src/containers/ThemeToggle/index.tsx
@@ -1,21 +1,15 @@
 import { FunctionComponent } from 'react';
-import { DefaultRootState, useDispatch, useSelector } from 'react-redux';
+import {  useDispatch } from 'react-redux';
 import { Dispatch } from 'redux';
 
 import SunAndMoon from '@src/components/SunAndMoon';
 import { themeHandler } from '@src/Redux/UI/uiActions';
 
 const ThemeToggle: FunctionComponent = () => {
-  const isLightTheme: boolean = useSelector(
-    (state: DefaultRootState) => state.ui.isLightTheme,
-  );
   const dispatch: Dispatch<any> = useDispatch();
 
   return (
-    <SunAndMoon
-      isLightTheme={isLightTheme}
-      onClick={() => dispatch(themeHandler())}
-    />
+    <SunAndMoon onClick={() => dispatch(themeHandler())} />
   );
 };
 
