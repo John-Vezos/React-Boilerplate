b47f85ff0b55c8a9f2b5bc1b020049fd68858cbd
John-Vezos <vezos@hotmail.gr>
2023-03-29 03:36:02 +0300

    Update index.tsx

--- src/components/SunAndMoon/index.tsx
+++ src/components/SunAndMoon/index.tsx
@@ -3,7 +3,6 @@ import SvgSunAndMoon from '@src/assets/icons/SunAndMoon&Moon';
 import { ThemeToggleStyle } from './ThemeToggleStyle';
 
 interface Props {
-  isLightTheme: boolean;
   onClick: (dispatchValue: { type: string }) => void;
 }
 
